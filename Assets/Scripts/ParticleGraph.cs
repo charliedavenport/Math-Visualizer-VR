878f86ba7e8cb6e2ae48fd1d51cc6b1486776df2
Charlie <cdavenport090@gmail.com>
2018-02-25 22:09:41 -0500

    Added VectorField + minor changes

--- Assets/Scripts/ParticleGraph.cs
+++ Assets/Scripts/ParticleGraph.cs
@@ -62,6 +62,12 @@ public class ParticleGraph : MonoBehaviour {
         return part;
     }
 
+    private Vector3 normalize_pos(Vector3 pos) {
+        return new Vector3(pos.x / Mathf.Max(Mathf.Abs(x_max), Mathf.Abs(x_min)) * graph_scale_factor,
+            pos.y / Mathf.Max(Mathf.Abs(y_max), Mathf.Abs(y_min)) * graph_scale_factor,
+            pos.z / Mathf.Max(Mathf.Abs(z_max), Mathf.Abs(z_min)) * graph_scale_factor);
+    }
+
     IEnumerator spin_particles() {
         while (true) {
             ps.GetParticles(particles);
@@ -110,37 +116,34 @@ public class ParticleGraph : MonoBehaviour {
 
         Debug.Log("n_particles: " + n_particles);
 
-        particles = new ParticleSystem.Particle[n_particles * 2];
+        particles = new ParticleSystem.Particle[n_particles];
 
         float x_val = x_min;
         float z_val = z_min;
         float incr = 1f / resolution;
 
-		GraphFunc func = cos_x_sin_z;
+        GraphFunc func = cos_x_sin_z;
         //GraphFunc func_1 = sphere_bottom;
 
-        // all this needs to happen on the GPU. oh boy
-        // TODO: write vertex shader for computing positions of particles
         int i = 0;
 
-            for (int z = 0; z < n_particles_z; z++) {
-                x_val = x_min;
-                for (int x = 0; x < n_particles_x; x++) {
-                    particles[i].position = new Vector3(x_val / x_max * graph_scale_factor, 
-                        func(x_val, z_val) / y_max * graph_scale_factor,
-                        z_val / z_max * graph_scale_factor);
-                    if (particles[i].position.y < y_min || particles[i].position.y > y_max) {
-                        particles[i].startColor = Color.clear; // particles outside of y-bounds are invisible
-                    }
-                    else particles[i].startColor =
-                            Color.HSVToRGB(particles[i].position.y / (y_max*2) + 0.5f, 1, 1); //normalize [-ymax, ymax] to [0,1]
-                    //particles[i].startColor = new Color(particles[i].startColor.r, particles[i].startColor.g, particles[i].startColor.b, 0.5f);
-                    particles[i].startSize = 0.02f;
-                    x_val += incr;
-                    i++;
+        for (int z = 0; z < n_particles_z; z++) {
+            x_val = x_min;
+            for (int x = 0; x < n_particles_x; x++) {
+                particles[i].position = normalize_pos(new Vector3(x_val, func(x_val, z_val), z_val));
+
+                if (particles[i].position.y < y_min || particles[i].position.y > y_max) {
+                    particles[i].startColor = Color.clear; // particles outside of y-bounds are invisible
                 }
-                z_val += incr;
+                else particles[i].startColor =
+                        Color.HSVToRGB((particles[i].position.y - y_min) / (y_max - y_min), 1, 1); //normalize [ymin, ymax] to [0,1]
+                //particles[i].startColor = new Color(particles[i].startColor.r, particles[i].startColor.g, particles[i].startColor.b, 0.5f);
+                particles[i].startSize = 0.1f;
+                x_val += incr;
+                i++;
             }
+            z_val += incr;
+        }
 
         ps.SetParticles(particles, particles.Length);
     }//Awake()
---
+++ Assets/Scripts/VectorField.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class VectorField : MonoBehaviour {
+
+
+    public int resolution = 10;
+    public int x_min, z_min, y_min, x_max, z_max, y_max;
+    public float scale_factor;
+
+    private int n_vectors_x, n_vectors_z, n_vectors_y;
+    private int n_vectors;
+
+    private ParticleSystem.Particle[] vectors;
+    private ParticleSystem ps;
+
+    private delegate Vector3 VectorFunc(Vector3 input);
+
+    // identity function: f(x) = x 
+    private static Vector3 identity(Vector3 input) {
+        return input;
+    }
+
+    private void Awake() {
+        ps = GetComponent<ParticleSystem>();
+
+        n_vectors_x = (x_max - x_min) * resolution;
+        n_vectors_y = (y_max - y_min) * resolution;
+        n_vectors_z = (z_max - z_min) * resolution;
+        n_vectors = n_vectors_x * n_vectors_y * n_vectors_z;
+
+        Debug.Log("n_vectors: " + n_vectors);
+
+        vectors = new ParticleSystem.Particle[n_vectors];
+
+        float x_val = x_min,
+            z_val = z_min,
+            y_val = y_min;
+        float incr = 1f / resolution;
+
+        VectorFunc func = identity;
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
