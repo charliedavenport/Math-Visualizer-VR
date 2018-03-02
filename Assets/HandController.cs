3ff35e76a0d356975f658d37f35fe738179d676a
melonbar <melonbar94@gmail.com>
2018-03-02 06:24:04 -0500

    changed player controls

--- Assets/HandController.cs
+++ Assets/HandController.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandController : MonoBehaviour {
+public class HandController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public VRPlayer player;
+
+    public Rigidbody intersected;
+    public bool isTeleporting = false;
+    public bool isTeleLocationValid = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (intersected == null)
+        {
+            intersected = other.attachedRigidbody;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        intersected = null;
+    }
+
+    public void joystick(Vector2 direction)
+    {
+
+    }
 }
+
