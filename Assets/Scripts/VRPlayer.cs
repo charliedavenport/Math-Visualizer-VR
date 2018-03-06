eacc7707c397075a8bbc9efc9f358a4566a9325c
melonbar <melonbar94@gmail.com>
2018-03-06 04:45:41 -0500

    added networking back

--- Assets/Scripts/VRPlayer.cs
+++ Assets/Scripts/VRPlayer.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.Networking;
 
-public class VRPlayer : MonoBehaviour
+/// <summary>
+/// /////////////////
+/// </summary>
+public class VRPlayer : NetworkBehaviour
 {
     //public GameLogic gameLogic;
 
@@ -13,39 +18,52 @@ public class VRPlayer : MonoBehaviour
     public GameManager gm;
 
     public Transform SteamVR_Rig;
-    //public Transform hmd;
-    //public Transform leftController;
-    //public Transform rightController;
+    public Transform hmd;
+    public Transform leftController;
+    public Transform rightController;
     public Transform feet;
     public SteamVR_TrackedObject hmd2;
     public SteamVR_TrackedObject controllerLeft;
     public SteamVR_TrackedObject controllerRight;
 
-    public Rigidbody leftHeldObj;
-    public Rigidbody rightHeldObj;
+    public enum LocomotionMode { TELEPORT };
+    public LocomotionMode locomotionMode = LocomotionMode.TELEPORT;
 
     float saveMaxLeft;
     float saveMaxRight;
 
     // Use this for initialization
+    [SyncVar]
+    Vector3 headPos;
+    [SyncVar]
+    Quaternion headRot;
+    [SyncVar]
+    Vector3 leftHandPos;
+    [SyncVar]
+    Quaternion leftHandRot;
+    [SyncVar]
+    Vector3 rightHandPos;
+    [SyncVar]
+    Quaternion rightHandRot;
     void Start()
     {
-       head.transform.position = new Vector3(0, 2, 0);
+        //  head.transform.position = new Vector3(0, 2, 0);
     }
 
     // Update is called once per frame
     void Update()
-    {/*
-        //int leftIndex = (int)leftController.GetComponent<SteamVR_TrackedObject>().index;
-        //int rightIndex = (int)rightController.GetComponent<SteamVR_TrackedObject>().index;
-       int leftIndex = (int)controllerLeft.index;
-        int rightIndex = (int)controllerRight.index;
+    {
+        int leftIndex = (int)leftController.GetComponent<SteamVR_TrackedObject>().index;
+        int rightIndex = (int)rightController.GetComponent<SteamVR_TrackedObject>().index;
+        // int leftIndex = (int)controllerLeft.index;
+        //int rightIndex = (int)controllerRight.index;
 
         // RIGHT HAND
         if (rightIndex >= 0)
         {
 
 
+            // Vector2 joyRight = getJoystick(controllerRight.transform);//rightController);
             Vector2 joyRight = getJoystick(rightController);
             rightHand.joystick(joyRight);
 
@@ -60,6 +78,7 @@ public class VRPlayer : MonoBehaviour
         // LEFT HAND
         if (leftIndex >= 0)
         {
+            // Vector2 joyLeft = getJoystick(controllerLeft.transform);//leftController);
             Vector2 joyLeft = getJoystick(leftController);
             leftHand.joystick(joyLeft);
 
@@ -67,10 +86,10 @@ public class VRPlayer : MonoBehaviour
 
 
         } // LEFT HAND
-		*/
+
     }
 
-    
+    /*
     private void FixedUpdate()
     {
         if (isLocalPlayer)
@@ -124,32 +143,31 @@ public class VRPlayer : MonoBehaviour
             rightHand.transform.rotation = rightHandRot;
 
         }
-    }
-	/*
+    }*///mel
+
     private void LateUpdate()
     {
-        head.position = hmd2.transform.position;
-        leftHand.transform.position = controllerLeft.transform.position;
-        rightHand.transform.position = controllerRight.transform.position;
+        head.position = hmd.position;
+        leftHand.transform.position = leftController.position;
+        rightHand.transform.position = rightController.position;
 
-        head.rotation = hmd2.transform.rotation;
+        head.rotation = hmd.rotation;
         leftHand.transform.rotation = leftController.transform.rotation;
         rightHand.transform.rotation = rightController.transform.rotation;
     } // LateUpdate
-	*/
 
     /**
      * Assigns network id 
      */
-      [Command]
+    /*  [Command]
       public void CmdGetAuthority(NetworkIdentity id)
       {
           id.AssignClientAuthority(this.connectionToClient);
-      }
+      }*/ //mel
           /**
            *Syncs players 
            */
-     [Command]
+    /* [Command]
      void CmdSyncPlayer(Vector3 pos, Quaternion rot, Vector3 lhpos, Quaternion lhrot, Vector3 rhpos, Quaternion rhrot)
      {
          head.transform.position = pos;
@@ -165,12 +183,12 @@ public class VRPlayer : MonoBehaviour
          rightHandPos = rhpos;
          rightHandRot = rhrot;
 
-     }
+     }*/ //mel
 
     /**
      * Copies Transform
      */
-     private void copyTransform(Transform origT, Transform newT)
+    /* private void copyTransform(Transform origT, Transform newT)
      {
          newT.position = origT.position;
          newT.rotation = origT.rotation;
@@ -181,7 +199,7 @@ public class VRPlayer : MonoBehaviour
          int indexLeft = (int)controllerLeft.index;
          int indexRight = (int)controllerRight.index;
 
- 
+ *///mel
 
 
     /*if (indexRight >= 0)
@@ -192,11 +210,13 @@ public class VRPlayer : MonoBehaviour
         }
     }*/
 
-      leftHand.controllerVelocity = getControllerVelocity(controllerLeft);
+    /*   leftHand.controllerVelocity = getControllerVelocity(controllerLeft);
        rightHand.controllerVelocity = getControllerVelocity(controllerRight);
        leftHand.controllerAngularVelocity = getControllerAngularVelocity(controllerLeft);
        rightHand.controllerAngularVelocity = getControllerAngularVelocity(controllerRight);
 
+       float triggerLeft = getTrigger(controllerLeft);
+       float triggerRight = getTrigger(controllerRight);
 
        Vector2 joyLeft = getJoystick(controllerLeft);
        Vector2 joyRight = getJoystick(controllerRight);
@@ -206,20 +226,20 @@ public class VRPlayer : MonoBehaviour
        //rightHand.squeeze(triggerRight);
 
        switch (locomotionMode)
-       { 
+       {*/ //mel
            /*case LocomotionMode.FLYING:
                {
                    fly(joyLeft, joyRight);
                    break;
                }*/
-           case LocomotionMode.TELEPORT:
+           /*case LocomotionMode.TELEPORT:
                            {
                                leftHand.joystick(joyLeft);
                                rightHand.joystick(joyRight);
                                break;
                            }
                    }
-               }
+               }*///mel
 
 
     private float getTrigger(SteamVR_TrackedObject controller)
@@ -234,13 +254,13 @@ public class VRPlayer : MonoBehaviour
         }
     }
 
-    private Vector2 getJoystick(SteamVR_TrackedObject controller)//(SteamVR_TrackedObject controller) //mel
+    private Vector2 getJoystick(Transform controller)//(SteamVR_TrackedObject controller) //mel
     {
         Debug.Log("in getJoystick");
-       /* if ((int)controller.GetComponent<SteamVR_TrackedObject>().index >= 0)
+        if ((int)controller.GetComponent<SteamVR_TrackedObject>().index >= 0)
             return SteamVR_Controller.Input((int)controller.GetComponent<SteamVR_TrackedObject>().index).GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
         else
-            return Vector2.zero;*/
+            return Vector2.zero;/*
 
         if (controller.index >= 0)
         {
@@ -249,7 +269,7 @@ public class VRPlayer : MonoBehaviour
         else
         {
             return Vector2.zero;
-        }
+        }*///mel
     }
     private Vector3 getControllerVelocity(SteamVR_TrackedObject controller)
     {
@@ -262,4 +282,37 @@ public class VRPlayer : MonoBehaviour
         Vector3 angularVelocity = controller.index >= 0 ? SteamVR_Controller.Input((int)controller.index).angularVelocity : Vector3.zero;
         return SteamVR_Rig.localToWorldMatrix.MultiplyVector(angularVelocity.normalized) * angularVelocity.magnitude;
     }
+
+    /* public void teleport(Transform controller)//Vector3 pos, Vector3 forward)
+     {
+         //hmdBlinker.blink(.1f);
+         // Vector3 facingDirection = new Vector3(head.forward.x, 0, head.forward.z);
+         // float angleBetween = Vector3.SignedAngle(facingDirection, forward, Vector3.up);
+         //   this.transform.Rotate(Vector3.up, angleBetween, Space.World);
+         // Vector3 offset = pos - feet.position;
+         //  this.transform.Translate(offset, Space.World);
+
+         Vector3 offset = pos - feet.position;
+         SteamVR_Rig.Translate(offset, Space.World);
+         Vector3 facingDirection = new Vector3(head.forward.x, 0, head.forward.z);
+         float angleBetween = Vector3.SignedAngle(facingDirection, forward, Vector3.up);
+         SteamVR_Rig.Rotate(Vector3.up, angleBetween, Space.World);
+     }*/ //mel
+         /*public void fly(Vector2 leftJoystick, Vector2 rightJoystick)
+         {
+
+             float leftSpeed = Mathf.Clamp(leftJoystick.y, 0, 1);
+             float rightSpeed = Mathf.Clamp(rightJoystick.y, 0, 1);
+             Vector3 leftDirection = leftHand.transform.forward;
+             Vector3 rightDirection = rightHand.transform.forward;
+             Vector3 displacement = (leftDirection * leftSpeed + rightDirection * rightSpeed) * Time.deltaTime;
+             this.transform.Translate(displacement, Space.World);
+             int index = (int)controller.GetComponent<SteamVR_TrackedObject>().index;
+             if (index >= 0)
+                 return SteamVR_Controller.Input(index).GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
+             else
+                 return Vector2.zero;
+         }*/
 }
+
+
