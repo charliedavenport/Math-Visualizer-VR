e76b08705efd814077594b47b00a4eb84f8f11ba
Charlie <cdavenport090@gmail.com>
2018-03-06 03:07:46 -0500

    Fixed all the things

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -48,138 +48,26 @@ public class HandController : MonoBehaviour
     }
 
     // visualization of teleporter
-    /*  public void joystick(Vector2 direction)
-      {
-          float mag = direction.magnitude;
-          float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
-          //angle = 0 means fully pushed up, 90/-270 means pushed to the left
-          // bool validLocation = false;
-          Vector3 hitPos = Vector3.zero;
-          Vector3 hitDir = Vector3.forward;
-          int currTelePoint = 0;
-          teleHitVisual.SetActive(true);
-
-          for (int i = 0; i < telePoints.Count; i++)
-          {
-              telePoints[i].SetActive(false);
-          }
-          //  teleHitVisual.SetActive(false);
-
-          if (isTeleporting)
-          {
-
-              //calculate arc points/distance
-              float distanceTraveled = 0.0f;
-              Vector3 currentPos = teleporterBase.position;
-              Vector3 currentVel = teleporterBase.forward * teleporterArcSpeed;
-              float deltaTime = teleporterArcSpeed / 500.0f;
-
-              while (distanceTraveled < maxDistance)
-              {
-                  //draw ray
-                  if (telePoints.Count <= currTelePoint)
-                  {
-                      GameObject go = GameObject.Instantiate<GameObject>(telePointPrefab);
-                      go.transform.SetParent(this.transform);
-                      telePoints.Add(go);
-                  } // add points to teleporter if it's not big enough
-
-                  Vector3 nextPos = currentPos + currentVel * deltaTime + .5f * teleporterArcGravity * deltaTime * deltaTime;
-                  Vector3 nextVel = currentVel + teleporterArcGravity * deltaTime; // needed because acceleration
-                  Vector3 between = nextPos - currentPos;
-
-                  telePoints[currTelePoint].SetActive(true);
-                  telePoints[currTelePoint].transform.position = currentPos;
-                  telePoints[currTelePoint].transform.forward = between.normalized;
-
-                  //draw ray
-
-
-                  //time to form our raycast
-
-                  RaycastHit hit;
-                  if (Physics.Raycast(new Ray(currentPos, between.normalized), out hit, between.magnitude))
-                  { // vector from current pos towards the next one will go as far as distance between
-
-                      hitPos = hit.point;
-                      isTeleLocationValid = true;
-                      teleHitVisual.SetActive(true);
-                      teleHitVisual.transform.position = hitPos;
-                      hitDir = new Vector3(teleporterBase.forward.x, 0, teleporterBase.forward.z);
-                      hitDir = Quaternion.AngleAxis(-angle, Vector3.up) * hitDir;
-                      // rotates by hit directrion to allow angle of joystick to change facing direction
-                      teleHitVisual.transform.forward = hitDir;
-                      //orients hit visusalization 
-                      break; // break to not continue once something is hit
-                  } //enters if something is hit with raycast
-
-
-                  currTelePoint++;
-                  currentPos = nextPos;
-                  currentVel = nextVel;
-                  distanceTraveled += between.magnitude;
-
-              }
-
-          } // do the drawing and physics raycasting
-
-
-
-
-
-
-
-          if (mag > .9f && !isTeleporting)
-          {
-              isTeleporting = true;
-          }
-          else if (mag < .85f && isTeleporting && isTeleLocationValid)
-          {
-              isTeleporting = false;
-              player.teleport(hitPos, hitDir);
-          }
-
-
-      }
-
-  */
-
     public void joystick(Vector2 direction)
     {
         float mag = direction.magnitude;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
         //angle = 0 means fully pushed up, 90/-270 means pushed to the left
-<<<<<<< HEAD
-<<<<<<< HEAD
-        Vector3 hitPos;
-=======
-=======
->>>>>>> parent of 59ab4a8... pls work
-       // bool validLocation = false;
+        // bool validLocation = false;
         Vector3 hitPos = Vector3.zero;
         Vector3 hitDir = Vector3.forward;
->>>>>>> parent of 59ab4a8... pls work
         int currTelePoint = 0;
         teleHitVisual.SetActive(true);
 
         for (int i = 0; i < telePoints.Count; i++)
         {
             telePoints[i].SetActive(false);
-<<<<<<< HEAD
-<<<<<<< HEAD
         }
-=======
-        } 
-      //  teleHitVisual.SetActive(false);
->>>>>>> parent of 59ab4a8... pls work
-=======
-        } 
-      //  teleHitVisual.SetActive(false);
->>>>>>> parent of 59ab4a8... pls work
+        //  teleHitVisual.SetActive(false);
 
         if (isTeleporting)
         {
-           
+
             //calculate arc points/distance
             float distanceTraveled = 0.0f;
             Vector3 currentPos = teleporterBase.position;
@@ -191,45 +79,50 @@ public class HandController : MonoBehaviour
                 //draw ray
                 if (telePoints.Count <= currTelePoint)
                 {
-                    GameObject go = GameObject.Instantiate<GameObject>(telePrefab);
+                    GameObject go = GameObject.Instantiate<GameObject>(telePointPrefab);
                     go.transform.SetParent(this.transform);
                     telePoints.Add(go);
                 } // add points to teleporter if it's not big enough
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-                
->>>>>>> parent of 59ab4a8... pls work
-=======
-                
->>>>>>> parent of 59ab4a8... pls work
+
                 Vector3 nextPos = currentPos + currentVel * deltaTime + .5f * teleporterArcGravity * deltaTime * deltaTime;
                 Vector3 nextVel = currentVel + teleporterArcGravity * deltaTime; // needed because acceleration
                 Vector3 between = nextPos - currentPos;
 
                 telePoints[currTelePoint].SetActive(true);
                 telePoints[currTelePoint].transform.position = currentPos;
-                telePoints[currTelePoint].transform.forward = between.normalized; 
+                telePoints[currTelePoint].transform.forward = between.normalized;
 
+                //draw ray
+                /*if(telePoints.Count <= currTelePoint){
+                    GaneObject go = GameObject.Instantiate<GameObject>(teleporterPointPrefab);
+                    go.transform.SetParent(this.transform);
+                    telePoints.Add(go);
+                }*/
 
                 //time to form our raycast
 
                 RaycastHit hit;
                 if (Physics.Raycast(new Ray(currentPos, between.normalized), out hit, between.magnitude))
                 { // vector from current pos towards the next one will go as far as distance between
+
                     hitPos = hit.point;
                     isTeleLocationValid = true;
                     teleHitVisual.SetActive(true);
                     teleHitVisual.transform.position = hitPos;
-                    teleHitVisual.transform.forward = new Vector3(teleporterBase.forward.x, 0, teleporterBase.forward.y);
+                    hitDir = new Vector3(teleporterBase.forward.x, 0, teleporterBase.forward.z);
+                    hitDir = Quaternion.AngleAxis(-angle, Vector3.up) * hitDir;
+                    // rotates by hit directrion to allow angle of joystick to change facing direction
+                    teleHitVisual.transform.forward = hitDir;
                     //orients hit visusalization 
                     break; // break to not continue once something is hit
                 } //enters if something is hit with raycast
+
 
                 currTelePoint++;
                 currentPos = nextPos;
                 currentVel = nextVel;
                 distanceTraveled += between.magnitude;
+
             }
 
         } // do the drawing and physics raycasting
@@ -240,14 +133,16 @@ public class HandController : MonoBehaviour
 
 
 
-        if (mag > .1f && !isTeleporting)
+        if (mag > .9f && !isTeleporting)
         {
             isTeleporting = true;
         }
-        else if (mag < .05f && isTeleporting && isTeleLocationValid)
+        else if (mag < .85f && isTeleporting && isTeleLocationValid)
         {
             isTeleporting = false;
+            player.teleport(hitPos, hitDir);
         }
+
 
     }
 
--- Assets/Scripts/VectorField.cs
+++ Assets/Scripts/VectorField.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(ParticleSystem))]
+[RequireComponent(typeof(ParticleSystem), typeof(LineRenderer))]
 public class VectorField : MonoBehaviour {
 
 
@@ -16,6 +16,7 @@ public class VectorField : MonoBehaviour {
 
     private ParticleSystem.Particle[] vectors;
     private ParticleSystem ps;
+    private LineRenderer lr;
 
     public delegate Vector3 VectorFunc(Vector3 input);
     public VectorFunc func;
@@ -25,36 +26,47 @@ public class VectorField : MonoBehaviour {
         return input;
     }
 
-    private static Vector3 spiral_up(Vector3 input)
-    {
+    private static Vector3 sprial_up(Vector3 input) {
         return new Vector3(
-            Mathf.Cos(input.x),
+            -input.z,
             0.5f,
-            Mathf.Sin(input.z));
+            input.x);
     }
 
-    private Vector3 normalize_pos(Vector3 pos)
-    {
+    private static Vector3 hyperbolic(Vector3 input) {
+        return new Vector3(
+            input.y * input.z,
+            input.x * input.z,
+            input.y * input.x);
+    }
+
+    private static Vector3 fluid_flow(Vector3 input) {
+        return new Vector3(
+            Mathf.Sin(input.x) + Mathf.Sin(input.y) + Mathf.Sin(input.z),
+            Mathf.Sin(input.x) - Mathf.Sin(input.y) + Mathf.Sin(input.z),
+            Mathf.Sin(input.x) + Mathf.Sin(input.y) - Mathf.Sin(input.z));
+    }
+
+    private Vector3 normalize_pos(Vector3 pos) {
         return new Vector3(pos.x / Mathf.Max(Mathf.Abs(x_max), Mathf.Abs(x_min)) * scale_factor,
             pos.y / Mathf.Max(Mathf.Abs(y_max), Mathf.Abs(y_min)) * scale_factor,
             pos.z / Mathf.Max(Mathf.Abs(z_max), Mathf.Abs(z_min)) * scale_factor);
     }
 
-    public void generate()
-    {
+    public void generate() {
+        float min_size = 0.05f,
+            max_size = 1f;
+
         float x_val = x_min,
             z_val = z_min,
             y_val = y_min;
         float incr = 1f / resolution;
         int i = 0;
-        for (int x = 0; x < n_vectors_x; x++)
-        {
+        for (int x = 0; x < n_vectors_x; x++) {
             y_val = y_min;
-            for (int y = 0; y < n_vectors_y; y++)
-            {
+            for (int y = 0; y < n_vectors_y; y++) {
                 z_val = z_min;
-                for (int z = 0; z < n_vectors_z; z++)
-                {
+                for (int z = 0; z < n_vectors_z; z++) {
                     Vector3 input_vec = new Vector3(x_val, y_val, z_val);
                     vectors[i].position = normalize_pos(input_vec);
                     Vector3 return_vec = func(input_vec);
@@ -62,9 +74,10 @@ public class VectorField : MonoBehaviour {
                     float phi = Mathf.Atan2(return_vec.y, Mathf.Sqrt(return_vec.x * return_vec.x + return_vec.z * return_vec.z)) * Mathf.Rad2Deg;
                     //Debug.Log("phi " + phi + ", theta " + theta);
                     vectors[i].rotation3D = new Vector3(-phi, theta, 0);
-                    float vec_size = start_size * Vector3.Magnitude(return_vec);
+                    float vec_size = Mathf.Clamp(start_size * Vector3.Magnitude(return_vec), min_size, max_size);
                     vectors[i].startSize3D = new Vector3(vec_size, vec_size, vec_size);
-                    vectors[i].startColor = Color.HSVToRGB(Vector3.Magnitude(return_vec) / (2f * y_max), 1, 1); // normalizing color values based on twice the y_max value
+                    float hue = Mathf.Clamp(Vector3.Magnitude(return_vec) / (2f * y_max), 0f, 1f);
+                    vectors[i].startColor = Color.HSVToRGB(hue, 1, 1); // normalizing color values based on twice the y_max value
                     z_val += incr;
                     i++;
                 }
@@ -77,9 +90,10 @@ public class VectorField : MonoBehaviour {
         ps.SetParticles(vectors, vectors.Length);
     }
 
-    private void Awake()
-    {
+    private void Awake() {
         ps = GetComponent<ParticleSystem>();
+        lr = GetComponent<LineRenderer>();
+        //lr.positionCount = 20;
 
         n_vectors_x = (x_max - x_min) * resolution + 1;
         n_vectors_y = (y_max - y_min) * resolution + 1;
@@ -90,10 +104,32 @@ public class VectorField : MonoBehaviour {
 
         vectors = new ParticleSystem.Particle[n_vectors];
 
-        func = spiral_up;
+        func = hyperbolic;
 
         generate(); // set particles
-
+        StartCoroutine(draw_solution(new Vector3(1f, -1f, 0.8f)));
     }//Awake()
+
+    IEnumerator draw_solution(Vector3 start_pos) {
+        Vector3 pos = start_pos;
+        //Vector3[] positions = new Vector3[10];
+        for (int i = 0; i < 40; i++) {
+            //positions[i] = pos;
+            lr.positionCount = i + 1;
+            pos += 0.1f * func(pos);
+            //Debug.Log(pos);
+            lr.SetPosition(i, normalize_pos(pos));
+
+            //break out if out of bounds
+            if (pos.x >= x_max || pos.x <= x_min || pos.y >= y_max || pos.y <= y_min || pos.z >= z_max || pos.z <= z_min)
+                break;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    }
+
+    private void Start() {
+        //StartCoroutine(draw_solution(new Vector3(0.5f, 0f, 0f)));
+    }
 
 }
