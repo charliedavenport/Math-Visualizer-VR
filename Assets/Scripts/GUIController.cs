d420d1f193e50c495a3776c3c10e4a7b47a12455
charlie <ced89538@uga.edu>
2018-03-05 23:14:46 -0500

    GUI stuff

--- Assets/Scripts/GUIController.cs
+++ Assets/Scripts/GUIController.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUIController : MonoBehaviour {
 
+	public Transform player;
+	public Text mode_text;
+	public Text rotate_text;
+	public Text func_text;
+
+	private RectTransform rect;
+
+	private bool isVectorMode; // true: vector field // false: particle graph
+	private string current_func;
+	private float current_rot;
+
 	// Use this for initialization
 	void Start () {
-		
+		isVectorMode = false;
+		rect = GetComponent<RectTransform>();
+		rect.rotation = Quaternion.Euler(45f, 0f, 0f);
+		rect.Translate(0f, 0f, 0.15f); // 'recenter' GUI with controller
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		mode_text.text = "Mode: " + (isVectorMode? "Vector Field" : "3D Graph");
+		rotate_text.text = "Rotation: " + current_rot.ToString("F");
+		func_text.text = "Function: " + current_func;
 	}
 }
---
+++ Assets/Scripts/GraphManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphManager : MonoBehaviour {
+
+	public bool isVectorMode = false;
+	public ParticleGraph particleGraph;
+	public VectorField vectorField;
+
+	// Use this for initialization
+	void Start () {
+		if (isVectorMode) {
+			particleGraph.gameObject.SetActive(false);
+			vectorField.gameObject.SetActive(true);
+		}
+		else {
+			particleGraph.gameObject.SetActive(true);
+			vectorField.gameObject.SetActive(false);
+		}
+	}
+	
+	// enable or disable grpahs
+	void setMode(bool isVec) {
+		isVectorMode = isVec;
+		if (isVectorMode) {
+			particleGraph.gameObject.SetActive(false);
+			vectorField.gameObject.SetActive(true);
+		}
+		else {
+			particleGraph.gameObject.SetActive(true);
+			vectorField.gameObject.SetActive(false);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
